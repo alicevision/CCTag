e816bdfedc94a26e12849e551010951ef2f5b961
Carsten Griwodz <griff@ifi.uio.no>
2015-02-02 17:48:19 +0100

    solve gomp

--- cmake/FindGomp.cmake
+++ cmake/FindGomp.cmake
@@ -1,2 +1,6 @@
+find_package(OpenMP REQUIRED)
+
+# griff: CMake own OpenMP package solves the problem for gcc and other compilers
 #TODO
-set(Gomp_LIBRARIES "/usr/lib/gcc/x86_64-linux-gnu/4.7/libgomp.so")
+# set(Gomp_LIBRARIES "/usr/lib/gcc/x86_64-linux-gnu/4.7/libgomp.so")
+
