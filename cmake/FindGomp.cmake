9d644bb4d75c9c88f3449d075bc213b3d81c99d3
Carsten Griwodz <griff@ifi.uio.no>
2015-02-17 15:18:36 +0100

    correct msg flag

--- cmake/FindGomp.cmake
+++ cmake/FindGomp.cmake
@@ -3,7 +3,7 @@ find_package(OpenMP REQUIRED)
 # griff: CMake own OpenMP package solves the problem for gcc and other compilers
 #TODO
 if(APPLE)
-  message( INFO "MacOSX: use -fopenmp instead of linking gomp" )
+  message( STATUS "MacOSX: use -fopenmp instead of linking gomp" )
 else(APPLE)
   # set(Gomp_LIBRARIES "/usr/lib/gcc/x86_64-linux-gnu/4.7/libgomp.so")
   set(Gomp_LIBRARIES "gomp" )
