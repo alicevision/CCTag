a692f0ae56e382895351b4d1cdfc898b59c1d19f
Lilian Calvet <lilian.calvet@gmail.com>
2015-02-27 11:35:34 +0100

    Clean/format EdgePoint.*

--- src/cctag/EdgePoint.cpp
+++ src/cctag/EdgePoint.cpp
@@ -1,17 +1,14 @@
 #include "EdgePoint.hpp"
 
-namespace popart {
-namespace vision {
+namespace popart
+{
+namespace vision
+{
 
 std::ostream& operator<<( std::ostream& os, const EdgePoint& eP )
 {
-
-	//Point2dN<int> xy eP.Point2dN();
-	//Point2dN<double> grad = eP.gradient();
-
-	os << "quiver( " << eP.x() << " , " << eP.y() << "," << eP._grad.x()/50 << "," << eP._grad.y()/50 << " ); ";
-
-	return os;
+  os << "quiver( " << eP.x() << " , " << eP.y() << "," << eP._grad.x()/50 << "," << eP._grad.y()/50 << " ); ";
+  return os;
 }
 
 }
--- src/cctag/EdgePoint.hpp
+++ src/cctag/EdgePoint.hpp
@@ -1,107 +1,99 @@
 #ifndef _CCTAG_VISION_EDGEPOINT_HPP_
 #define _CCTAG_VISION_EDGEPOINT_HPP_
 
-#include "geometry/point.hpp"
+#include <cctag/geometry/point.hpp>
 
 #include <cstddef>
 #include <sys/types.h>
 #include <cmath>
 #include <iosfwd>
 
-namespace popart {
-namespace vision {
+namespace popart
+{
+namespace vision
+{
 
 class Label;
 
 class EdgePoint : public popart::Point2dN<int>
 {
 public:
-	EdgePoint()
-		: popart::Point2dN<int>( 0, 0 )
-		, _normGrad( -1.0 )
-		, _before( NULL )
-		, _after( NULL )
-		, _processed( -1 )
-		, _processedIn( false )
-		, _isMax( -1 )
-		, _edgeLinked( -1 )
-		, _nSegmentOut(-1)
-		, _flowLength (0)
-		,_processedAux(false)
+  EdgePoint()
+    : popart::Point2dN<int>( 0, 0 )
+    , _normGrad( -1.0 )
+    , _before( NULL )
+    , _after( NULL )
+    , _processed( -1 )
+    , _processedIn( false )
+    , _isMax( -1 )
+    , _edgeLinked( -1 )
+    , _nSegmentOut(-1)
+    , _flowLength (0)
+    ,_processedAux(false)
 
-	{
-		//_vfieldLineRatio.reserve(4) ;// check a global variable from configation todo@Lilian
-	}
+  {}
 
-	EdgePoint( const EdgePoint& p )
-		: popart::Point2dN<int>( p )
-		, _grad( p._grad )
-	    , _normGrad ( p._normGrad )
-		, _before( p._before )
-		, _after( p._after )
-		, _processed( -1 )
-	    , _processedIn( false )
-		, _isMax( -1 )
-		, _edgeLinked( -1 )
-		, _nSegmentOut(-1)
-		, _flowLength (0)
-		, _processedAux(false)
-	{
-		//_vfieldLineRatio.reserve(4);// check a global variable from configation todo@Lilian
-	}
+  EdgePoint( const EdgePoint& p )
+    : popart::Point2dN<int>( p )
+    , _grad( p._grad )
+    , _normGrad ( p._normGrad )
+    , _before( p._before )
+    , _after( p._after )
+    , _processed( -1 )
+    , _processedIn( false )
+    , _isMax( -1 )
+    , _edgeLinked( -1 )
+    , _nSegmentOut(-1)
+    , _flowLength (0)
+    , _processedAux(false)
+  {}
 
-	EdgePoint( const int vx, const int vy, const float vdx, const float vdy )
-		: popart::Point2dN<int>( vx, vy )
-		, _before( NULL )
-		, _after( NULL )
-		, _processed( -1 )
-	    , _processedIn( false )
-		, _isMax( -1 )
-		, _edgeLinked( -1 )
-		, _nSegmentOut(-1)
-		, _flowLength (0)
-		, _processedAux(false)
-	{
-		//_vfieldLineRatio.reserve(4);// check a global variable from configation todo@Lilian
-		_normGrad = std::sqrt( vdx * vdx + vdy * vdy );
-		_grad = popart::Point2dN<double>( (double) vdx , (double) vdy );
-	}
+  EdgePoint( const int vx, const int vy, const float vdx, const float vdy )
+    : popart::Point2dN<int>( vx, vy )
+    , _before( NULL )
+    , _after( NULL )
+    , _processed( -1 )
+    , _processedIn( false )
+    , _isMax( -1 )
+    , _edgeLinked( -1 )
+    , _nSegmentOut(-1)
+    , _flowLength (0)
+    , _processedAux(false)
+  {
+    _normGrad = std::sqrt( vdx * vdx + vdy * vdy );
+    _grad = popart::Point2dN<double>( (double) vdx , (double) vdy );
+  }
 
-	virtual ~EdgePoint() {}
+  virtual ~EdgePoint() {}
 
-	inline popart::Point2dN<double> gradient() const { return _grad ; }
+  inline popart::Point2dN<double> gradient() const
+  {
+    return _grad ;
+  }
 
-	inline double normGradient() const { return _normGrad ; }
+  inline double normGradient() const
+  {
+    return _normGrad ;
+  }
 
-	friend std::ostream& operator<<( std::ostream& os, const EdgePoint& eP );
+  friend std::ostream& operator<<( std::ostream& os, const EdgePoint& eP );
 
-	popart::Point2dN<double> _grad;
-	double _normGrad;
-	EdgePoint* _before;
-	EdgePoint* _after;
-	ssize_t _processed;
-	bool _processedIn;
-	ssize_t _isMax;
-	ssize_t _edgeLinked;
-	std::size_t _nSegmentOut;
-	float _flowLength;
-	bool _processedAux;
-	//boost::numeric::ublas::vector<double> _vfieldLineRatio;
-        
-        
-        //template<class Archive>
-	//void serialize(Archive & ar, const unsigned int version)
-	//{
-	//	ar & BOOST_SERIALIZATION_NVP( x() );
-	//	ar & BOOST_SERIALIZATION_NVP( y() );
-	//	ar & BOOST_SERIALIZATION_NVP( _grad.x() );
-	//	ar & BOOST_SERIALIZATION_NVP( _grad.y() );
-	//}
-        
+  popart::Point2dN<double> _grad;
+  double _normGrad;
+  EdgePoint* _before;
+  EdgePoint* _after;
+  ssize_t _processed;
+  bool _processedIn;
+  ssize_t _isMax;
+  ssize_t _edgeLinked;
+  std::size_t _nSegmentOut;
+  float _flowLength;
+  bool _processedAux;
 };
 
-inline bool receivedMoreVoteThan(const EdgePoint * const p1,  const EdgePoint * const p2){
-    return (p1->_isMax > p2->_isMax);
+inline bool receivedMoreVoteThan(const EdgePoint * const p1,  const EdgePoint * const p2)
+{
+  return (p1->_isMax > p2->_isMax);
 }
 
 }
