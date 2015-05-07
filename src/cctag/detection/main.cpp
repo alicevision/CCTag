4079fe727fe90e76a82985267a07b805409c2597
Lilian Calvet <lilian.calvet@gmail.com>
2015-05-07 14:19:54 +0200

    Add video mode - end.

--- src/cctag/detection/main.cpp
+++ src/cctag/detection/main.cpp
@@ -10,7 +10,6 @@
 #include <cctag/view.hpp>
 #include <cctag/image.hpp>
 
-
 #include <boost/filesystem/convenience.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/progress.hpp>
@@ -46,28 +45,34 @@ static const std::string kUsageString = "Usage: detection image_file.png\n";
 void detection(std::size_t frame, cctag::View& view, const std::string & paramsFilename = "")
 {
     POP_ENTER;
+    // Set the system parameters
+    cctag::vision::marker::Parameters params;
+    
+    if (paramsFilename != "") {
+      // Read the parameter file provided by the user
+      std::ifstream ifs(paramsFilename.c_str());
+      boost::archive::xml_iarchive ia(ifs);
+      ia >> boost::serialization::make_nvp("CCTagsParams", params);
+      std::cout << "Parameters contained in " << paramsFilename << " are used." << std::endl;
+    } else {
+      // Use the default parameters and save them in defaultParameters.xml
+      std::ofstream ofs("defaultParameters.xml");
+      boost::archive::xml_oarchive oa(ofs);
+      oa << boost::serialization::make_nvp("CCTagsParams", params);
+      std::cout << "Parameter file not provided. Default parameters are used." << std::endl;
+    }
+    
     // Process markers detection
     boost::timer t;
     boost::ptr_list<marker::CCTag> markers;
-    cctag::vision::marker::Parameters params;
-    if (paramsFilename != "") {
-        std::ifstream ifs(paramsFilename.c_str());
-        boost::archive::xml_iarchive ia(ifs);
-        // write class instance to archive
-        ia >> boost::serialization::make_nvp("CCTagsParams", params);
-    } else {
-        std::ofstream ofs("detectionCCTagParams.xml");
-        boost::archive::xml_oarchive oa(ofs);
-        // write class instance to archive
-        oa << boost::serialization::make_nvp("CCTagsParams", params);
-    }
 
     view.setNumLayers( params._numberOfMultiresLayers );
     
-    ROM_COUT("beforecctagDetection");
     cctagDetection( markers, frame, view._grayView, params, true );
-    ROM_COUT("aftercctagDetection");
 
+    ROM_TCOUT( markers.size() << " markers.");
+    ROM_TCOUT("Total time: " << t.elapsed());
+    
     std::cout << "Id : ";
 
     int i = 0;
@@ -83,9 +88,6 @@ void detection(std::size_t frame, cctag::View& view, const std::string & paramsF
         ++i;
     }
     std::cout << std::endl;
-
-    ROM_TCOUT( markers.size() << " markers.");
-    ROM_TCOUT("Total time: " << t.elapsed());
     POP_LEAVE;
 }
 
@@ -111,125 +113,74 @@ int main(int argc, char** argv)
         const bfs::path subFilenamePath(myPath.filename());
         const bfs::path parentPath(myPath.parent_path());
         std::string ext(extPath.string());
+        
+        // Create inputImagePath/result if it does not exist
+        std::stringstream resultFolderName, localizationFolderName, identificationFolderName;
+        resultFolderName << parentPath.string() << "/result";
+        localizationFolderName << resultFolderName.str() << "/localization";
+        identificationFolderName << resultFolderName.str() << "/identification";
 
-        bfs::path folder(argv[1]);
+        if (!bfs::exists(resultFolderName.str())) {
+            bfs::create_directory(resultFolderName.str());
+        }
 
-        if ((ext == ".png") || (ext == ".jpg")) {
+        if (!bfs::exists(localizationFolderName.str())) {
+            bfs::create_directory(localizationFolderName.str());
+        }
 
-            cctag::View my_view( filename );
+        if (!bfs::exists(identificationFolderName.str())) {
+            bfs::create_directory(identificationFolderName.str());
+        }
 
-            rgb8_image_t& image = my_view._image;
-            rgb8_view_t&  svw   = my_view._view;
+        if ( (ext == ".png") || (ext == ".jpg") ) {
 
-            // Increase image size.
-            /*{
-                    rgb8_image_t simage;
-                    simage.recreate( 2 * image.width(), 2 * image.height() );
-                    rgb8_view_t osvw( view( simage ) );
-                    terry::resize_view( svw, osvw, terry::sampler::bicubic_sampler() );
-            }*/
+          cctag::View my_view( filename );
 
-            // Create inputImagePath/result if it does not exist
-            std::stringstream resultFolderName, localizationFolderName, identificationFolderName;
-            resultFolderName << parentPath.string() << "/result";
-            localizationFolderName << resultFolderName.str() << "/localization";
-            identificationFolderName << resultFolderName.str() << "/identification";
+          rgb8_image_t& image = my_view._image;
+          rgb8_view_t&  svw   = my_view._view;
 
-            if (!bfs::exists(resultFolderName.str())) {
-                bfs::create_directory(resultFolderName.str());
-            }
+          // Increase image size.
+          /*{
+                  rgb8_image_t simage;
+                  simage.recreate( 2 * image.width(), 2 * image.height() );
+                  rgb8_view_t osvw( view( simage ) );
+                  terry::resize_view( svw, osvw, terry::sampler::bicubic_sampler() );
+          }*/
 
-            if (!bfs::exists(localizationFolderName.str())) {
-                bfs::create_directory(localizationFolderName.str());
-            }
+          resultFolderName << "/" << myPath.stem().string();
 
-            if (!bfs::exists(identificationFolderName.str())) {
-                bfs::create_directory(identificationFolderName.str());
-            }
+          if (!bfs::exists(resultFolderName.str())) {
+            bfs::create_directory(resultFolderName.str());
+          }
+        
+          POP_INFO << "using result folder " << resultFolderName.str() << std::endl;
+          POP_INFO << "looking at image " << myPath.stem().string() << std::endl;
 
-            // Set the output image name, holding the same name as the original image
-            // so that the result will be placed in the inputImagePath/result/ folder
-            std::stringstream strstreamResult;
-            strstreamResult << resultFolderName.str() << "/" << myPath.stem().string() << ".png";
-            ROM_TCOUT(strstreamResult.str());
+          CCTagVisualDebug::instance().initBackgroundImage(svw);
+          CCTagVisualDebug::instance().initPath(resultFolderName.str());
+          CCTagVisualDebug::instance().setImageFileName(myPath.stem().string());
+          CCTagFileDebug::instance().setPath(resultFolderName.str());
 
-            resultFolderName << "/" << myPath.stem().string();
+          detection(0, my_view, paramsFilename);
 
-            if (!bfs::exists(resultFolderName.str())) {
-                bfs::create_directory(resultFolderName.str());
-            }
-            POP_INFO << "using result folder " << resultFolderName.str() << std::endl;
-            POP_INFO << "looking at image " << myPath.stem().string() << std::endl;
-
-            CCTagVisualDebug::instance().initBackgroundImage(svw);
-            CCTagVisualDebug::instance().initPath(resultFolderName.str());
-            CCTagVisualDebug::instance().setImageFileName(myPath.stem().string());
-
-            CCTagFileDebug::instance().setPath(resultFolderName.str());
-
-            detection(0, my_view, paramsFilename);
-
-            CCTagFileDebug::instance().outPutAllSessions();
-            CCTagFileDebug::instance().clearSessions();
-
-            // write visual file output
-            CCTagVisualDebug::instance().outPutAllSessions();
-        } else if (bfs::is_directory(folder)) {
-          // todo@Lilian: does not work
-            std::cout << folder << " is a directory containing:\n";
-
-            std::vector<bfs::path> filesInFolder;
-
-            copy(bfs::directory_iterator(folder), bfs::directory_iterator(), back_inserter(filesInFolder)); // is directory_entry, which is
-            // converted to a path by the
-            // path stream inserter
-
-            sort(filesInFolder.begin(), filesInFolder.end());
-            std::size_t frame = 0;
-
-            BOOST_FOREACH(const bfs::path & fileInFolder, filesInFolder) {
-                ROM_TCOUT(fileInFolder);
-
-                const std::string ext(bfs::extension(fileInFolder));
-                std::stringstream outFilename, inFilename;
-
-                if (ext == ".png") {
-                    inFilename << argv[3] << "/orig/" << std::setfill('0') << std::setw(5) << frame << ".png";
-                    outFilename << argv[3] << "/" << std::setfill('0') << std::setw(5) << frame << ".png";
-
-                    cctag::View my_view( inFilename.str() );
-                    rgb8_image_t& image      = my_view._image;
-                    rgb8_view_t&  sourceView = my_view._view;
-
-                    // rgb8_image_t image(png_read_dimensions(fileInFolder.c_str()));
-                    // rgb8_view_t sourceView(view(image));
-                    // png_read_and_convert_view(fileInFolder.c_str(), sourceView);
-
-                    POP_INFO << "writing input image to " << inFilename.str() << std::endl;
-                    png_write_view(inFilename.str(), sourceView);
-
-                    CCTagVisualDebug::instance().initBackgroundImage(sourceView);
-
-                    detection(frame, my_view, paramsFilename);
-
-                    POP_INFO << "writing ouput image to " << outFilename.str() << std::endl;
-                    png_write_view(outFilename.str(), sourceView);
-
-                    ++frame;
-                }
-            }
-        } else if (ext == ".avi" ){// || ext == ".mts" || ext == ".mov") {
+          CCTagFileDebug::instance().outPutAllSessions();
+          CCTagFileDebug::instance().clearSessions();
+          CCTagVisualDebug::instance().outPutAllSessions();
+          CCTagVisualDebug::instance().clearSessions();
+          
+        } else if (ext == ".avi" )
+        {
           std::cout << "*** Video mode ***" << std::endl;
           
           // open video and check
           cv::VideoCapture video(filename.c_str());
-        if(!video.isOpened()) {std::cout << "Unable to open the video : " << filename; return -1;}
+          if(!video.isOpened()) {std::cout << "Unable to open the video : " << filename; return -1;}
 
-        // play loop
-        int lastFrame = video.get(CV_CAP_PROP_FRAME_COUNT);
-        int frameId = 0;
-        while( video.get(CV_CAP_PROP_POS_FRAMES) < lastFrame )
-        {
+          // play loop
+          int lastFrame = video.get(CV_CAP_PROP_FRAME_COUNT);
+          int frameId = 0;
+          while( video.get(CV_CAP_PROP_POS_FRAMES) < lastFrame )
+          {
             cv::Mat frame;
             video >> frame;
             cv::Mat imgGray;
@@ -237,12 +188,29 @@ int main(int argc, char** argv)
             cctag::View cctagView((const unsigned char *) imgGray.data, imgGray.cols, imgGray.rows , imgGray.step );
             cctagView._view = boost::gil::interleaved_view(imgGray.cols, imgGray.rows, (boost::gil::rgb8_pixel_t*) frame.data, frame.step );
 
+            // Write output
+            std::stringstream outFileName, outFolderName;
+            outFileName << std::setfill('0') << std::setw(5) << frameId;
+            outFolderName << resultFolderName.str() << "/" << outFileName.str();
+
+            if (!bfs::exists(outFolderName.str())) {
+                bfs::create_directory(outFolderName.str());
+            }
+            
+            CCTagVisualDebug::instance().initBackgroundImage(cctagView._view);
+            CCTagVisualDebug::instance().initPath(outFolderName.str());
+            CCTagVisualDebug::instance().setImageFileName(outFileName.str());
+            CCTagFileDebug::instance().setPath(outFolderName.str());
+            
             detection(frameId, cctagView);
             
-            png_write_view("/home/lilian/data/test.png", cctagView._view);
-            ++frameId;
-        }
-        
+            CCTagFileDebug::instance().outPutAllSessions();
+            CCTagFileDebug::instance().clearSessions();
+            CCTagVisualDebug::instance().outPutAllSessions();
+            CCTagVisualDebug::instance().clearSessions();
+            
+            ++frameId; 
+          }
         } else {
             throw std::logic_error("Unrecognized input.");
         }
