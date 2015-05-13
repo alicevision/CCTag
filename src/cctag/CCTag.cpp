70fb10aa25ef6bf8b79ef16be73e9d025be137ce
Lilian Calvet <lilian.calvet@gmail.com>
2015-05-13 13:19:43 +0200

    Fix header

--- src/cctag/CCTag.cpp
+++ src/cctag/CCTag.cpp
@@ -7,7 +7,6 @@
 #include <cctag/algebra/matrix/operation.hpp>
 #include <cctag/geometry/distance.hpp>
 #include <cctag/optimization/conditioner.hpp>
-#include <cctag/viewGeometry/homography.hpp>
 #include <cctag/viewGeometry/2DTransform.hpp>
 
 #ifdef WITH_CMINPACK
