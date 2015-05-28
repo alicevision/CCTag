9b33cb316c8a3c93bd5c4ccd2758ecd5ab140c5b
Carsten Griwodz <griff@ifi.uio.no>
2015-05-28 16:50:40 +0200

    draft of gradient descent

--- src/cuda/frame_gaussian.cu
+++ src/cuda/frame_gaussian.cu
@@ -115,6 +115,27 @@ __device__ __constant__ unsigned char d_thinning_lut_t[256];
 
 #define V7_WIDTH    32
 
+__device__
+inline
+bool outOfBounds( int x, int y, const cv::cuda::PtrStepSzb& edges )
+{
+    return ( x < 0 || x >= edges.cols || y < 0 || y >= edges.rows );
+}
+
+__device__
+template <typename T>
+inline T d_abs( T value )
+{
+    return ( ( value < 0 ) ? -value : value );
+}
+
+__device__
+template <typename T>
+inline int d_sign( T value )
+{
+    return ( ( value < 0 ) ? -1 : 1 );
+}
+
 template <class DestType>
 __global__
 void filter_gauss_horiz( cv::cuda::PtrStepSzf          src,
@@ -206,13 +227,12 @@ void compute_mag_l1( cv::cuda::PtrStepSz16s src_dx,
     int idx     = block_x + threadIdx.x;
     int idy     = blockIdx.y;
 
-    if( idx >= dst.cols ) return;
-    if( idy >= dst.rows ) return;
+    if( outOfBounds( idx, idy, dst ) ) return;
 
     int16_t dx = src_dx.ptr(idy)[idx];
     int16_t dy = src_dy.ptr(idy)[idx];
-    dx = (dx < 0 ) ? -dx : dx;
-    dy = (dy < 0 ) ? -dy : dy;
+    dx = d_abs( dx );
+    dy = d_abs( dy );
     dst.ptr(idy)[idx] = dx + dy;
 }
 
@@ -225,8 +245,7 @@ void compute_mag_l2( cv::cuda::PtrStepSz16s src_dx,
     int idx     = block_x + threadIdx.x;
     int idy     = blockIdx.y;
 
-    if( idx >= dst.cols ) return;
-    if( idy >= dst.rows ) return;
+    if( outOfBounds( idx, idy, dst ) ) return;
 
     int16_t dx = src_dx.ptr(idy)[idx];
     int16_t dy = src_dy.ptr(idy)[idx];
@@ -250,18 +269,18 @@ void compute_map( const cv::cuda::PtrStepSz16s dx,
     const int idx     = block_x + threadIdx.x;
     const int idy     = blockIdx.y;
 
-    if( idx >= dx.cols ) return;
-    if( idy >= dx.rows ) return;
+    if( outOfBounds( idx, idy, dx ) ) return;
 
     int32_t  dxVal  = dx.ptr(idy)[idx];
     int32_t  dyVal  = dy.ptr(idy)[idx];
     uint32_t magVal = mag.ptr(idy)[idx];
 
     // -1 if only is negative, 1 else
-    const int32_t signVal = (dxVal ^ dyVal) < 0 ? -1 : 1;
+    // const int32_t signVal = (dxVal ^ dyVal) < 0 ? -1 : 1;
+    const int32_t signVal = d_sign( dxVal ^ dyVal );
 
-    dxVal = (dxVal < 0 ) ? -dxVal : dxVal;
-    dyVal = (dyVal < 0 ) ? -dyVal : dyVal;
+    dxVal = d_abs( dxVal );
+    dyVal = d_abs( dyVal );
 
     // 0 - the pixel can not belong to an edge
     // 1 - the pixel might belong to an edge
@@ -318,16 +337,12 @@ void edge_hysteresis( cv::cuda::PtrStepSzb map, cv::cuda::PtrStepSzb edges )
     edges.ptr(idy)[idx] = log;
 }
 
-__global__
-void thinning( cv::cuda::PtrStepSzb src, cv::cuda::PtrStepSzb dst, bool first_run )
+__device__
+bool thinning_inner( const int idx, const int idy, cv::cuda::PtrStepSzb src, cv::cuda::PtrStepSzb dst, bool first_run )
 {
-    const int block_x = blockIdx.x * V7_WIDTH;
-    const int idx     = block_x + threadIdx.x;
-    const int idy     = blockIdx.y;
-
     uint8_t log = 0;
 
-    if( src.ptr(idy)[idx] == 0 ) return;
+    if( src.ptr(idy)[idx] == 0 ) return false;
 
     if( idx >= 1 && idy >=1 && idx <= src.cols-2 && idy <= src.rows-2 ) {
         log |= ( src.ptr(idy-1)[idx-1] != 0 ) ? 0x80 : 0;
@@ -353,8 +368,207 @@ void thinning( cv::cuda::PtrStepSzb src, cv::cuda::PtrStepSzb dst, bool first_ru
 
         dst.ptr(idy)[idx] = d_thinning_lut[log];
 #endif
+        return true;
+    }
+    return false;
+}
+
+__global__
+void thinning( cv::cuda::PtrStepSzb src, cv::cuda::PtrStepSzb dst )
+{
+    const int block_x = blockIdx.x * V7_WIDTH;
+    const int idx     = block_x + threadIdx.x;
+    const int idy     = blockIdx.y;
+
+    thinning_inner( idx, idy, src, dst, true );
+}
+
+void thinning_and_store( cv::cuda::PtrStepSzb src, cv::cuda::PtrStepSzb dst, uint32_t* edgeCounter, uint32_t edgeMax, int2* edgeCoords )
+{
+    const int block_x = blockIdx.x * V7_WIDTH;
+    const int idx     = block_x + threadIdx.x;
+    const int idy     = blockIdx.y;
+
+    bool keep = thinning_inner( idx, idy, src, dst, false );
+
+    uint32_t mask = __balloc( keep );  // bitfield of warps with results
+    uint32_t ct   = __popc( mask );    // horizontal reduce
+    uint32_t leader = __ffs(mask) - 1; // the highest thread id with indicator==true
+    uint32_t write_index;
+    if( threadIdx.x == leader ) {
+        // leader gets warp's offset from global value and increases it
+        write_index = atomicAdd( edgeCounter, ct );
+    }
+    write_index = __shfl( write_index, leader ); // broadcast warp write index to all
+    write_index += __popc( mask & ((1 << threadIdx.x) - 1) ); // find own write index
+
+    if( keep && write_index < edgeMax ) {
+        edgeCoords[write_index] = make_int4( idx, idy );
     }
 }
+
+__device__
+void updateXY(const float & dx, const float & dy, int & x, int & y,  float & e, int & stpX, int & stpY)
+{
+    float d = dy / dx;
+    float a = d_abs( d );
+    // stpX = ( dx < 0 ) ? -1 : ( dx == 0 ) ? 0 : 1;
+    // stpY = ( dy < 0 ) ? -1 : ( dy == 0 ) ? 0 : 1;
+    // stpX = ( dx < 0 ) ? -1 : 1;
+    // stpY = ( dy < 0 ) ? -1 : 1;
+    stpX = d_sign( dx );
+    stpY = d_sign( dy );
+    e   += a;
+    x   += stpX;
+    if( e >= 0.5 ) {
+        y += stpY;
+        e -= 1.0f;
+    }
+}
+
+__device__
+bool gradiant_descent_inner( int4&                  out_edge_info,
+                             int2*                  d_edgelist,
+                             uint32_t               edgeCount,
+                             cv::cuda::PtrStepSzb   edges,
+                             // int                    direction,
+                             uint32_t               nmax,
+                             cv::cuda::PtrStepSz16s d_dx,
+                             cv::cuda::PtrStepSz16s d_dy,
+                             int32_t                thrGradient )
+{
+    const int offset    = blockIdx.x * 32 + threadIdx.y;
+    const int direction = threadIdx.x == 0 ? 1 : -1;
+
+    if( offset >= edgeCount ) return false;
+
+    const int idx = d_edgelist[offset].x;
+    const int idy = d_edgelist[offset].y;
+    // const int block_x = blockIdx.x * V7_WIDTH;
+    // const int idx     = block_x + threadIdx.x;
+    // const int idy     = blockIdx.y;
+
+    if( outOfBounds( idx, idy, edges ) ) return false; // should never happen
+
+    if( edges.ptr(idy)[idx] == 0 ) return false; // should never happen
+
+    float  e     = 0.0f;
+    float  dx    = direction * d_dx.ptr(idy)[idx];
+    float  dy    = direction * d_dy.ptr(idy)[idx];
+    const float  adx   = d_abs( dx );
+    const float  ady   = d_abs( dy );
+    size_t n     = 0;
+    int    stpX  = 0;
+    int    stpY  = 0;
+    int    x     = idx;
+    int    y     = idy;
+
+    if( ady > adx ) {
+        updateXY(dy,dx,y,x,e,stpY,stpX);
+    } else {
+        updateXY(dx,dy,x,y,e,stpX,stpY);
+    }
+    n += 1;
+    if ( dx*dx+dy*dy > thrGradient ) {
+        const float dxRef = dx;
+        const float dyRef = dy;
+        const float dx2 = d_dx.ptr(idy)[idx];
+        const float dy2 = d_dy.ptr(idy)[idx];
+        const float compdir = dx2*dxRef+dy2*dyRef;
+        // dir = ( compdir < 0 ) ? -1 : 1;
+        dir = d_sign( compdir );
+        dx = dir * dx2;
+        dy = dir * dy2;
+    }
+    if( ady > adx ) {
+        updateXY(dy,dx,y,x,e,stpY,stpX);
+    } else {
+        updateXY(dx,dy,x,y,e,stpX,stpY);
+    }
+    n += 1;
+
+    if( outOfBounds( x, y, edges ) ) return false;
+
+    ret = edges.ptr(y)[x];
+    if( ret ) {
+        out_edge_info = make_int4( idx, idy, x, y );
+        return true;
+    }
+    
+    while( n <= nmax ) {
+        if( ady > adx ) {
+            updateXY(dy,dx,y,x,e,stpY,stpX);
+        } else {
+            updateXY(dx,dy,x,y,e,stpX,stpY);
+        }
+        n += 1;
+
+        if( outOfBounds( x, y, edges ) ) return false;
+
+        ret = edges.ptr(y)[x];
+        if( ret ) {
+            out_edge_info = make_int4( idx, idy, x, y );
+            return true;
+        }
+
+        if( ady > adx ) {
+            if( outOfBounds( x, y - stpY, edges ) ) return false;
+
+            ret = edges.ptr(y-stpY)[x];
+            if( ret ) {
+                out_edge_info = make_int4( idx, idy, x, y-stpY );
+                return true;
+            }
+        } else {
+            if( outOfBounds( x - stpX, y, edges ) ) return false;
+
+            ret = edges.ptr(y)[x-stpX];
+            if( ret ) {
+                out_edge_info = make_int4( idx, idy, x-stpX, y );
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+__global__
+void gradiant_descent( int2*                  d_edgelist,
+                       uint32_t               edgeCount,
+                       int4*                  d_new_edgelist,
+                       uint32_t*              d_new_edge_counter,
+                       cv::cuda::PtrStepSzb   edges,
+                       uint32_t               nmax,
+                       cv::cuda::PtrStepSz16s d_dx,
+                       cv::cuda::PtrStepSz16s d_dy,
+                       int32_t                thrGradient )
+{
+    int4 out_edge_info;
+    bool keep = gradiant_descent_inner( out_edge_info,
+                                        d_edgelist,
+                                        edgeCount,
+                                        edges,
+                                        nmax,
+                                        d_dx,
+                                        d_dy,
+                                        thrGradient );
+
+    uint32_t mask = __balloc( keep );  // bitfield of warps with results
+    uint32_t ct   = __popc( mask );    // horizontal reduce
+    uint32_t leader = __ffs(mask) - 1; // the highest thread id with indicator==true
+    uint32_t write_index;
+    if( threadIdx.x == leader ) {
+        // leader gets warp's offset from global value and increases it
+        write_index = atomicAdd( d_new_edge_counter, ct );
+    }
+    write_index = __shfl( write_index, leader ); // broadcast warp write index to all
+    write_index += __popc( mask & ((1 << threadIdx.x) - 1) ); // find own write index
+
+    if( keep && write_index < edgeMax ) {
+        d_new_edgelist[write_index] = out_edge_info;
+    }
+}
+
 
 #if 0
 __global__
@@ -451,12 +665,86 @@ void Frame::applyGauss( )
 
     thinning
         <<<grid,block,0,_stream>>>
-        ( _d_edges, cv::cuda::PtrStepSzb(_d_intermediate), true );
+        ( _d_edges, cv::cuda::PtrStepSzb(_d_intermediate) );
 
-    thinning
+    uint32_t edge_counter = 0;
+    POP_CUDA_MEMCPY_ASYNC( _d_edge_counter, &edge_count, sizeof(uint32_t), cudaMemcpyHostToDevice, _stream );
+
+    thinning_and_store
         <<<grid,block,0,_stream>>>
-        ( cv::cuda::PtrStepSzb(_d_intermediate), _d_edges, false );
+        ( cv::cuda::PtrStepSzb(_d_intermediate), _d_edges, _d_edge_counter, _edge_max, _d_edgelist );
 
+    POP_CUDA_MEMCPY_ASYNC( &edge_counter, _d_edge_counter, sizeof(uint32_t), cudaMemcpyDeviceToHost, _stream );
+
+    // Note: right here, Dynamic Parallelism would avoid blocking.
+    cudaStreamSynchronize( _stream );
+    // Try to figure out how that can be done with CMake.
+
+    const uint32_t nmax      = params._distSearch;
+    const int32_t  threshold = params._thrGradientMagInVote;
+    block.x = 2;
+    block.y = 32;
+    block.z = 0;
+    grid.x  = edge_counter / 32 + ( edge_counter & 0x1f != 0 ? 1 : 0 );
+
+    {
+        uint32_t dummy = 0;
+        POP_CUDA_MEMCPY_ASYNC( _d_edge_counter, &dummy, sizeof(uint32_t), cudaMemcpyHostToDevice, _stream );
+    }
+
+    gradiant_descent
+        <<<grid,block,0,_stream>>>
+        ( _d_edgelist, edge_counter, _d_edgelist_2, _d_edge_counter, _d_edges, nmax, _d_dx, _d_dy, threshold, _d_out_points );
+
+    POP_CUDA_MEMCPY_ASYNC( &edge_counter, _d_edge_counter, sizeof(uint32_t), cudaMemcpyDeviceToHost,_stream );
+
+    // Note: right here, Dynamic Parallelism would avoid blocking.
+    cudaStreamSynchronize( _stream );
+    // Try to figure out how that can be done with CMake.
+
+    /*
+     * After this:
+     * - it is unclear whether a sweep over the entire edge plane
+     *   is still an efficient choice, or whether it would be better
+     *   to reduce the edge points into a flat array first.
+     * - if reduction is the thing to do, it would be better to
+     *   combine it with the second thinning step.
+     *
+     * cctagDetectionFromEdges takes as parameters:
+     * - markers     : output, list of marker coordinates
+     * - points      : a vector that contains all edge points (input)
+     * - sourceView  : image at this scale (input)
+     * - cannyGradX  : _d_dx (input)
+     * - cannyGradY  : _d_dy (input)
+     * - edgesMap    : _d_edges (input)
+     * - frame       : running counter for video frames
+     * - level       : this layer of the pyramid
+     * - scale       : probably a coordinate multiplier
+     * - params      : parameters
+     *
+     * calls vote
+     *
+     * vote takes as input:
+     * - points     : a vector that contains all edge points (input)
+     * - seeds      : unknown (output)
+     * - edgesMap   : _d_edges (input)
+     * - winners    : map of winners (output)
+     * - cannyGradX : _d_dx (input)
+     * - cannyGradY : _d_dy (input)
+     * - params     : parameters
+     *
+     * calls gradientDirectionDescent
+     *
+     * gradientDirectionDescent takes as input
+     * - canny       : _d_edges (input)
+     * - p           : coordinate of one edge point (input)
+     * - dir         : direction, 1 or -1
+     * - nmax        : global parameter the distance from edge point for searching
+     * - cannyGradX  : _d_dx
+     * - cannyGradY  : _d_dy
+     * - thrGradiant : global parameter for gradient thresholding
+     * returns: NULL or a new point
+     */
 #if 0
     // very costly printf-debugging
     debug_gauss
