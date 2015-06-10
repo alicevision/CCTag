3587fa75c84ecd45563be66fc3deafdf7aafc603
Lilian Calvet <lilian.calvet@gmail.com>
2015-06-10 17:21:29 +0200

    Fix ImageCut object.

--- src/cctag/imageCut.hpp
+++ src/cctag/imageCut.hpp
@@ -2,16 +2,34 @@
 #define	_CCTAG_LINE_HPP_
 
 #include <cctag/geometry/point.hpp>
+#include <cctag/params.hpp>
 
 #include <boost/numeric/ublas/vector.hpp>
 
 namespace cctag {
 
-struct ImageCut
+class ImageCut
 {
-	Point2dN<double> _start;
-	Point2dN<double> _stop;
-	boost::numeric::ublas::vector<double> _imgSignal; //< image signal
+  
+  ImageCut()
+  {
+    
+  }
+ 
+  ImageCut(Point2dN<double> pStart, Point2dN<double> pStop)
+  : _start(pStart), _stop(pStop)
+  {
+    _imgSignal.reserve(cctag::kDefaultSampleCutLength);
+  }
+  
+  virtual ~ImageCut()
+  {
+  }
+  
+  Point2dN<double> _start;
+  Point2dN<double> _stop;
+  boost::numeric::ublas::vector<double> _imgSignal; //< image signal
+  
 };
 
 }
