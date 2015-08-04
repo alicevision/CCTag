48049adacbfc2d0860ba3e9530621ab200d8b5d7
Carsten Griwodz <griff@simula.no>
2015-08-04 15:04:55 +0200

    debug atan result

--- src/cuda/frame_link.cu
+++ src/cuda/frame_link.cu
@@ -522,7 +522,7 @@ void edge_linking_seed( const TriplePoint*           p,
             
             if( not stop ) {
 #ifdef DEBUG_LINKED_USE_INT4_BUFFER
-                buf.append( direction, new_point, shifting, angle );
+                buf.append( direction, new_point, shifting, atanval*100 );
 #else // DEBUG_LINKED_USE_INT4_BUFFER
                 buf.append( direction, new_point );
 #endif // DEBUG_LINKED_USE_INT4_BUFFER
