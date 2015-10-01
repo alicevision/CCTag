ceb6ee7c48cb2bbbde73227049400967e4a5b340
Lilian Calvet <lilian.calvet@gmail.com>
2015-10-02 01:00:26 +0200

    Matlab eval: set default experiments to run.

--- matlab/syntheticExperiments/main.m
+++ matlab/syntheticExperiments/main.m
@@ -1,7 +1,7 @@
 % For the documentation, cf. the one in lc_displayAllResults.m
 
-iXp = 3; % in {1,2,3}
-patterns = { '4Crowns' };% Subset of {'3Crowns','4Crowns','ARTKPlus'}.
+iXp = 1; % in {1,2,3}
+patterns = { '3Crowns' };% Subset of {'3Crowns','4Crowns','ARTKPlus'}.
                              % Only one must be passed for branches
                              % comparison.
 branchNames = {'cpu_adapt_of_gpu_part', 'optim_identify'};
