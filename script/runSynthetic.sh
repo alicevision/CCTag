077fd6d7b4d049b5531606926010129f9d48782f
Lilian Calvet <lilian.calvet@gmail.com>
2015-10-05 17:45:53 +0200

    Add shell script to run the synthetic experiments.

---
+++ script/runSynthetic.sh
@@ -0,0 +1,7 @@
+CCTAG_SOFTWARE_PATH = /home/lcalvet/cpp_workspace/cctag
+IMAGE_PATH=$DATA_PATH"/cctag/bench/images/synthetic/"$1"Crowns/xp"$2"/images/*.png";
+echo $IMAGE_PATH
+for j in $IMAGE_PATH; do
+    .$CCTAG_SOFTWARE_PATH/build/$3/src/detection -i $j -n $1 $CCTAG_SOFTWARE_PATH/parameters/param$1.xml -o $3
+    echo .$CCTAG_SOFTWARE_PATH/build/$3/src/detection -i $j -n $1 $CCTAG_SOFTWARE_PATH/parameters/param$1.xml -o $3
+done
