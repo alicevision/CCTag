a4d0ade93f550ffc8a0245a0969dae71d636564e
Carsten Griwodz <griff@simula.no>
2015-10-07 11:27:25 +0200

    compare cpu_adapt_of_gpu_part and optim_identify_gpu_sepcompile

--- matlab/syntheticExperiments/main.m
+++ matlab/syntheticExperiments/main.m
@@ -6,7 +6,7 @@ iXp = 1; % in {1,2,3}
 patterns = { '3Crowns' };% Subset of {'3Crowns','4Crowns','ARTKPlus'}.
                              % Only one must be passed for branches
                              % comparison.
-branchNames = {'cpu_adapt_of_gpu_part', 'optim_identify'};
+branchNames = {'cpu_adapt_of_gpu_part', 'optim_identify_gpu_sepcompile'};
                              % It can be any of the available branches.
                              % If only one pattern is passed as input, then
                              % a couple of branches (a maximum of 2) can be passed for
