586f22bffc9d06d8a72f9641f17aaac9a5a44fc0
Lilian Calvet <lilian.calvet@gmail.com>
2015-10-13 20:05:54 +0200

    Test glob_optim against glob_optim_clean.

--- matlab/syntheticExperiments/main.m
+++ matlab/syntheticExperiments/main.m
@@ -6,8 +6,9 @@ iXp = 1; % in {1,2,3}
 patterns = { '3Crowns' };    % Subset of {'3Crowns','4Crowns','ARTKPlus'}.
                              % Only one must be passed for branches
                              % comparison.
-branchNames = {'cpu_adapt_of_gpu_part', 'glob_center_optim' }; %'optim_identify'};
                              % It can be any of the available branches.
+%branchNames = {'cpu_adapt_of_gpu_part', 'glob_center_optim' }; %'optim_identify'};
+branchNames = {'glob_center_optim', 'glob_center_optim_clean' }; %'optim_identify'};
                              % If only one pattern is passed as input, then
                              % a couple of branches (a maximum of 2) can be passed for
                              % comparison.
