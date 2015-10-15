aa40c7196d476ffbf0ad70f8ee5d4dc77e9bd85b
Lilian Calvet <lilian.calvet@gmail.com>
2015-10-15 14:31:34 +0200

    Refactor ImageCut.

--- src/cctag/ImageCut.hpp
+++ src/cctag/ImageCut.hpp
@@ -18,27 +18,73 @@ public:
   }
  
   ImageCut( Point2dN<double> pStart, DirectedPoint2d<double> pStop )
-  : _start(pStart), _stop(pStop)
+  : _start(pStart), _stop(pStop), _outOfBounds(false), _beginSig(0.0), _endSig(1.0)
+  {
+    _imgSignal.resize(cctag::kDefaultSampleCutLength);
+  }
+  
+  ImageCut( Point2dN<double> pStart, DirectedPoint2d<double> pStop, const double start, const double stop)
+  : _start(pStart), _stop(pStop), _outOfBounds(false), _beginSig(start), _endSig(stop)
   {
     _imgSignal.resize(cctag::kDefaultSampleCutLength);
     _outOfBounds = false;
   }
   
-    ImageCut( Point2dN<double> pStart, DirectedPoint2d<double> pStop, const std::size_t nSamples)
+  ImageCut( Point2dN<double> pStart, DirectedPoint2d<double> pStop, const std::size_t nSamples)
   : _start(pStart), _stop(pStop)
   {
     _imgSignal.resize(nSamples);
     _outOfBounds = false;
   }
-  
-  virtual ~ImageCut()
+    
+  ImageCut( Point2dN<double> pStart, DirectedPoint2d<double> pStop, const double start, const double stop, const std::size_t nSamples)
+  : _start(pStart), _stop(pStop), _outOfBounds(false), _beginSig(start), _endSig(stop)
   {
+    _imgSignal.resize(nSamples);
+    _outOfBounds = false;
   }
   
+  const Point2dN<double> & start() const { return _start; }
+  Point2dN<double> & start() { return _start; }
+  
+  const Point2dN<double> & stop() const { return _stop; }
+  Point2dN<double> & stop() { return _stop; }
+  
+  const boost::numeric::ublas::vector<double> & imgSignal() const { return imgSignal; }
+  boost::numeric::ublas::vector<double> & imgSignal() { return imgSignal; }
+  
+  double beginSig() { return _beginSig; }
+  
+  double endSig() { return _endSig; }
+  
+  void setOutOfBounds(const bool outOfBounds) { _outOfBounds = outOfBounds; }
+  
+  virtual ~ImageCut() {}
+  
+private:
+  
+  // Start point of the image cut
   Point2dN<double> _start;
+  
+  // Stop point of the image cut
   DirectedPoint2d<double> _stop;
+  
+  // 1D rectified image signal along the segment [_start,_stop]
   boost::numeric::ublas::vector<double> _imgSignal; //< image signal
+  
+  // False by default. This boolean reveals if any of the points lying on the segment
+  // [_start, _stop] are outside of the image
   bool _outOfBounds;
+  
+  // Scalar value in [0,1] representing from where, along the segment [_start,_stop] 
+  // whose extremities corresponds to 0 and 1 resp., the image signal in stored in 
+  // _imgSignal 
+  double _beginSig;
+  
+  // Scalar value in [_beginSig,1] representing to where, along the segment [_start,_stop] 
+  // whose extremities corresponds to 0 and 1 resp., the image signal in stored in 
+  // _imgSignal
+  double _endSig;
   
 };
 
