fbc4eb6a34ee0a9b616feb6c079cb40e7715d4b7
Carsten Griwodz <griff@simula.no>
2015-10-22 15:15:16 +0200

    vectorize hyst

--- src/cuda/frame_hyst.cu
+++ src/cuda/frame_hyst.cu
@@ -25,23 +25,10 @@ __device__
 inline
 uint32_t get( cv::cuda::PtrStepSz32u img, const int idx, const int idy )
 {
-#if 1
-    return img.ptr( clamp( idy, img.rows ) )[ clamp( idx, img.cols ) ];
-#else
-    int x = clamp( idx, img.cols );
-    int y = clamp( idy, img.rows );
-    assert( x >= 0 );
-    assert( y >= 0 );
-    assert( x < img.cols );
-    assert( y < img.rows );
-    uint8_t  val = img.ptr(y)[x];
-    if( val > 2 ) {
-        printf("idx=%d -> x=%d, idy=%d -> y=%d, img.cols=%d img.rows=%d val=%d\n",
-            idx, x, idy, y, img.cols, img.rows, val );
-        assert( val <= 2 );
+    if( idx < 0 || idy < 0 || idx >= img.cols || idy >= img.rows ) {
+        return 0;
     }
-    return val;
-#endif
+    return img.ptr(idy)[idx];
 }
 
 __device__
@@ -49,134 +36,156 @@ void load( cv::cuda::PtrStepSz32u img )
 {
     const int srcidx = blockIdx.x * HYST_W + threadIdx.x;
     const int srcidy = blockIdx.y * HYST_H + threadIdx.y;
+    uint32_t v;
 
-    uint32_t val_0_0;
-    val_0_0 = get( img, srcidx-1, srcidy-1 );
+    volatile uint32_t* load_line;
+    load_line = reinterpret_cast<volatile uint32_t*>(&array[threadIdx.y][0]);
 
-    volatile uint32_t* reinterpret_array;
-    reinterpret_array = reinterpret_cast<volatile uint32_t*>(&array[threadIdx.y][0]);
-    reinterpret_array[threadIdx.x  ] = val_0_0;
+    v = get( img, srcidx-1, srcidy-1 );
+    load_line[threadIdx.x  ] = v;
 
-    if( threadIdx.y >= HYST_H - 2 ) {
-        uint32_t val_2_0;
-        val_2_0 = get( img, srcidx-1, srcidy+1 );
-        reinterpret_array = reinterpret_cast<volatile uint32_t*>(&array[threadIdx.y+2][0]);
-        reinterpret_array[threadIdx.x  ] = val_2_0;
-        if( threadIdx.x >= HYST_W - 2 ) {
-            uint32_t val_2_2;
-            val_2_2 = get( img, srcidx+1, srcidy+1 );
-            reinterpret_array[threadIdx.x+2] = val_2_2;
-        }
+    if( threadIdx.x >= HYST_W - 2 ) {
+        v = get( img, srcidx+1, srcidy-1 );
+        load_line[threadIdx.x+2] = v;
     }
     __syncthreads();
-    if( threadIdx.x >= HYST_W - 2 ) {
-        uint32_t val_0_2;
-        val_0_2 = get( img, srcidx+1, srcidy-1 );
-        reinterpret_array = reinterpret_cast<volatile uint32_t*>(&array[threadIdx.y][0]);
-        reinterpret_array[threadIdx.x+2] = val_0_2;
+
+    if( threadIdx.y >= HYST_H - 2 ) {
+        load_line = reinterpret_cast<volatile uint32_t*>(&array[threadIdx.y+2][0]);
+
+        v = get( img, srcidx-1, srcidy+1 );
+        load_line[threadIdx.x  ] = v;
+
+        if( threadIdx.x >= HYST_W - 2 ) {
+            v = get( img, srcidx+1, srcidy+1 );
+            load_line[threadIdx.x+2] = v;
+        }
     }
     __syncthreads();
 }
 
 __device__
-void store( cv::cuda::PtrStepSz32u img )
+void store( cv::cuda::PtrStepSz32u img, bool printout )
 {
     const int dstidx  = blockIdx.x * HYST_W + threadIdx.x;
     const int dstidy  = blockIdx.y * HYST_H + threadIdx.y;
 
-    volatile uint32_t* reinterpret_array;
+#if 1
+    if( dstidx*sizeof(uint32_t) < img.step && dstidy < img.rows ) {
+        // volatile uint32_t* shared_line = reinterpret_cast<volatile uint32_t*>(array[threadIdx.y+1]);
+        volatile uint32_t* shared_line = reinterpret_cast<volatile uint32_t*>(&array[threadIdx.y+1][0]);
+        uint32_t val = shared_line[threadIdx.x+1];
 
-    volatile uint32_t val;
-    reinterpret_array = reinterpret_cast<volatile uint32_t*>(&array[threadIdx.y+1][0]);
-    val = reinterpret_array[threadIdx.x+1];
-
-    if( dstidx < img.cols && dstidy < img.rows ) {
-        img.ptr(dstidy)[dstidx] =  val;
+        img.ptr(dstidy)[dstidx] = val;
     }
+#else
+    union {
+        uint8_t  b[4];
+        uint32_t i;
+    } val;
+
+    val.b[0] = array[threadIdx.y+1][threadIdx.x*4+4];
+    val.b[1] = array[threadIdx.y+1][threadIdx.x*4+5];
+    val.b[2] = array[threadIdx.y+1][threadIdx.x*4+6];
+    val.b[3] = array[threadIdx.y+1][threadIdx.x*4+7];
+    if( printout ) {
+        printf("(%d,%d)<-%d  (%d,%d)<-%d  (%d,%d)<-%d  (%d,%d)<-%d\n",
+               dstidx*4+0, dstidy, val.b[0],
+               dstidx*4+1, dstidy, val.b[1],
+               dstidx*4+2, dstidy, val.b[2],
+               dstidx*4+3, dstidy, val.b[3]);
+    }
+    if( dstidx*sizeof(uint32_t) < img.step && dstidy < img.rows ) {
+        img.ptr(dstidy)[dstidx] = val.i;
+    }
+#endif
 }
 
 __device__
 inline
 bool update_edge_pixel( int y, int x )
 {
-    uint8_t val[3][3];
-    volatile uint8_t debug_v;
-    val[0][0] = array[y  ][x  ];
-    val[0][1] = array[y  ][x+1];
-    val[0][2] = array[y  ][x+2];
-    val[1][0] = array[y+1][x  ];
-    val[1][1] = array[y+1][x+1];
-    val[1][2] = array[y+1][x+2];
-    val[2][0] = array[y+2][x  ];
-    val[2][1] = array[y+2][x+1];
-    val[2][2] = array[y+2][x+2];
+    bool something_changed = false;
 
-    assert( val[0][0] <= 2 );
-    assert( val[0][1] <= 2 );
-    assert( val[0][2] <= 2 );
-    assert( val[1][0] <= 2 );
-    assert( val[1][1] <= 2 );
-    assert( val[1][2] <= 2 );
-    assert( val[2][0] <= 2 );
-    assert( val[2][1] <= 2 );
-    assert( val[2][2] <= 2 );
+    union {
+        uint8_t  b[12];
+        uint3    i;
+    } val[3];
 
-    bool inc = false;
-    bool dec = false;
+    val[0].i = make_uint3( reinterpret_cast<volatile uint32_t*>( &array[y  ][x] )[0],
+                           reinterpret_cast<volatile uint32_t*>( &array[y  ][x] )[1],
+                           reinterpret_cast<volatile uint32_t*>( &array[y  ][x] )[2] );
+    val[1].i = make_uint3( reinterpret_cast<volatile uint32_t*>( &array[y+1][x] )[0],
+                           reinterpret_cast<volatile uint32_t*>( &array[y+1][x] )[1],
+                           reinterpret_cast<volatile uint32_t*>( &array[y+1][x] )[2] );
+    val[2].i = make_uint3( reinterpret_cast<volatile uint32_t*>( &array[y+1][x] )[0],
+                           reinterpret_cast<volatile uint32_t*>( &array[y+2][x] )[1],
+                           reinterpret_cast<volatile uint32_t*>( &array[y+2][x] )[2] );
 
-    debug_v = val[1][1];
-    if( val[1][1] == 1 ) {
-        inc = ( val[0][0] == 2 || val[0][1] == 2 || val[0][2] == 2 ||
-                val[1][0] == 2 ||                   val[1][2] == 2 ||
-                val[2][0] == 2 || val[2][1] == 2 || val[2][2] == 2 );
-        dec = ( val[0][0] == 0 && val[0][1] == 0 && val[0][2] == 0 &&
-                val[1][0] == 0 &&                   val[1][2] == 0 &&
-                val[2][0] == 0 && val[2][1] == 0 && val[2][2] == 0 );
-        debug_v = val[1][1] = inc ? 2 : dec ? 0 : 1 ;
+    for( int i=0; i<4; i++ ) {
+        bool inc = false;
+        bool dec = false;
+
+        if( val[1].b[4+i] == 1 ) {
+            inc = ( val[0].b[3+i] == 2 || val[0].b[4+i] == 2 || val[0].b[5+i] == 2 ||
+                    val[1].b[3+i] == 2 ||                       val[1].b[5+i] == 2 ||
+                    val[2].b[3+i] == 2 || val[2].b[4+i] == 2 || val[2].b[5+i] == 2 );
+            dec = ( val[0].b[3+i] == 0 && val[0].b[4+i] == 0 && val[0].b[5+i] == 0 &&
+                    val[1].b[3+i] == 0 &&                       val[1].b[5+i] == 0 &&
+                    val[2].b[3+i] == 0 && val[2].b[4+i] == 0 && val[2].b[5+i] == 0 );
+            val[1].b[4+i] = inc ? 2 : dec ? 0 : 1 ;
+        }
+        __syncthreads();
+
+        something_changed |= inc;
+        something_changed |= dec;
     }
-    __syncthreads();
-    assert( debug_v == val[1][1] );
-    array[y+1][x+1] = val[1][1];
+    reinterpret_cast<volatile uint32_t*>( &array[y+1][x] )[1] = val[1].i.y;
 
-    return ( inc || dec );
+    return something_changed;
 }
 
 __device__
-bool edge_block_loop( )
+bool edge_block_loop( int debug_roundcount )
 {
     __shared__ volatile bool continuation[HYST_H];
     bool            again = true;
     bool            something_changed = false;
-    bool            line_changed = false;
-    int ct = 0;
+    int debug_inner_loop_count = 0;
 
-    while( again && ct < 10 ) {
-        assert( ct <= HYST_W*HYST_H );
-        bool mark = false;
-        mark = mark || update_edge_pixel( threadIdx.y, threadIdx.x    );
-        mark = mark || update_edge_pixel( threadIdx.y, threadIdx.x+32 );
-        mark = mark || update_edge_pixel( threadIdx.y, threadIdx.x+64 );
-        mark = mark || update_edge_pixel( threadIdx.y, threadIdx.x+96 );
+    // DEBUG NOTE:
+    // updating in the inner loop works correctly
+    // but the outer loop repeats exactly changes in the inner loop, in particular on
+    // x values of 0, 1 or 2
+    // WHY ?
 
-        /* make sure all updated pixel are written back to
-         * shared memory before continuation[] is modified */
-        __threadfence_block();
+    while( again ) { // && debug_inner_loop_count < 10 ) {
+        assert( debug_inner_loop_count <= HYST_W*HYST_H );
+
+        bool mark = update_edge_pixel( threadIdx.y, threadIdx.x*4 );
 
         /* every row checks whether any pixel has been changed */
-        line_changed = __any( mark );
+        bool line_changed = __any( mark );
 
+#if 0
         /* the first thread of each row write the result to continuation[] */
         if( threadIdx.x == 0 ) continuation[threadIdx.y] = line_changed;
+
+        /* make sure all updated pixel are written back to
+         * shared memory before continuation[] is modified.
+         * This is supposedly redundant with __syncthreads() */
+        __threadfence_block();
 
         /* wait for all rows to fulfill the operation (and to assure that
          * results in continuation[] are visible to all threads, because
          * threadfence() is implied by syncthreads() */
         __syncthreads();
 
-        /* Each thread in a warp reads continuation for one row.
+        /* Each thread in a warp reads __any() results for one of 32 warps.
          * Redundant, but I have no better idea for spreading the result
          * to all warps. */
-        mark = threadIdx.x < HYST_H ? continuation[threadIdx.x] : false;
+        // mark = threadIdx.x < HYST_H ? continuation[threadIdx.x] : false;
+        mark = continuation[threadIdx.x]; // each warp reads result for all 32 warps
 
         /* Finally, all 32x32 threads know whether at least one of them
          * has changed a pixel.
@@ -184,6 +193,19 @@ bool edge_block_loop( )
          * the change further.
          */
         again = __any( mark );
+#else
+        if( threadIdx.x == 0 ) continuation[threadIdx.y] = line_changed;
+        __syncthreads();
+        if( threadIdx.y == 0 ) {
+            mark = continuation[threadIdx.x];
+            again = __any(mark);
+            if( threadIdx.x == 0 ) {
+                continuation[0] = again;
+            }
+        }
+        __syncthreads();
+        again = continuation[0];
+#endif
 
         /* Every threads needs to know whether any pixel was changed in
          * any round of the loop because egde_second() uses this return
@@ -191,18 +213,21 @@ bool edge_block_loop( )
         if( again ) something_changed = true;
 
         /* this should not be necessary ... */
-        ct++;
+        debug_inner_loop_count++;
     }
 
     return something_changed;
 }
 
 __device__
-bool edge( int* block_counter )
+bool edge( int* block_counter, int debug_roundcount )
 {
-    bool something_changed = edge_block_loop( );
+    bool something_changed = edge_block_loop( debug_roundcount );
     if( threadIdx.x == 0 && threadIdx.y == 0 ) {
         if( something_changed ) {
+            if( debug_roundcount > 25 ) {
+                printf("Something changed in block (%d,%d)\n", blockIdx.x, blockIdx.y );
+            }
             atomicAdd( block_counter, 1 );
         }
     }
@@ -210,7 +235,7 @@ bool edge( int* block_counter )
 }
 
 __global__
-void edge_first( cv::cuda::PtrStepSzb img, int* block_counter, cv::cuda::PtrStepSzb src )
+void edge_first( cv::cuda::PtrStepSzb img, int* block_counter, cv::cuda::PtrStepSzb src, int debug_roundcount )
 {
     // const int idx  = blockIdx.x * HYST_W + threadIdx.x;
     // const int idy  = blockIdx.y * HYST_H + threadIdx.y;
@@ -224,7 +249,7 @@ void edge_first( cv::cuda::PtrStepSzb img, int* block_counter, cv::cuda::PtrStep
     input.cols = src.cols / 4;
     load( input );
 
-    edge( block_counter );
+    edge( block_counter, debug_roundcount );
 
     __syncthreads();
 
@@ -233,11 +258,11 @@ void edge_first( cv::cuda::PtrStepSzb img, int* block_counter, cv::cuda::PtrStep
     output.step = img.step;
     output.rows = img.rows;
     output.cols = img.cols / 4;
-    store( output );
+    store( output, false );
 }
 
 __global__
-void edge_second( cv::cuda::PtrStepSzb img, int* block_counter )
+void edge_second( cv::cuda::PtrStepSzb img, int* block_counter, int debug_roundcount )
 {
     cv::cuda::PtrStepSz32u input;
     input.data = reinterpret_cast<uint32_t*>(img.data);
@@ -247,10 +272,10 @@ void edge_second( cv::cuda::PtrStepSzb img, int* block_counter )
     input.cols = img.cols / 4;
     load( input );
 
-    bool something_changed = edge( block_counter );
+    bool something_changed = edge( block_counter, debug_roundcount );
 
     if( __any( something_changed ) ) {
-        store( input );
+        store( input, false );
     }
 }
 
@@ -295,7 +320,7 @@ void hyst_outer_loop( int width, int height, int* block_counter, cv::cuda::PtrSt
             grid.x, grid.y, grid.z, block.x, block.y, block.z );
 
     bool first_time = true;
-    int loopcount = 0;
+    int debug_roundcount = 0;
     do
     {
         *block_counter = 0;
@@ -304,19 +329,21 @@ void hyst_outer_loop( int width, int height, int* block_counter, cv::cuda::PtrSt
                 <<<grid,block>>>
                 ( img,
                   block_counter,
-                  src );
+                  src,
+                  debug_roundcount );
             first_time = false;
         } else {
             hysteresis::edge_second
                 <<<grid,block>>>
                 ( img,
-                  block_counter );
+                  block_counter,
+                  debug_roundcount );
         }
         cudaDeviceSynchronize( );
-        printf( "block_counter=%d\n", *block_counter);
+        printf( "width=%d height=%d block_counter=%d\n", width, height, *block_counter);
         assert( *block_counter <= grid.x * grid.y );
     }
-    while( loopcount++ < 30 ); // *block_counter > 0 );
+    while( *block_counter > 0 && debug_roundcount++ < 30 ); // *block_counter > 0 );
     printf( "Leave %s\n", __FUNCTION__ );
 }
 #endif // USE_SEPARABLE_COMPILATION
