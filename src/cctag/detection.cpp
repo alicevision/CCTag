1f217068dceb216b7781394da0d630a1e3adf9d1
Carsten Griwodz <griff@simula.no>
2015-12-04 13:40:54 +0100

    minor simplication

--- src/cctag/detection.cpp
+++ src/cctag/detection.cpp
@@ -620,11 +620,21 @@ void cctagDetectionFromEdges(
 
       quality2 *= scale;
 
-      markers.push_back(new CCTag(-1, outerEllipse.center(), cctagPoints,
-              outerEllipse, markerHomography, pyramidLevel, scale, quality2));
+      CCTag* tag = new CCTag( -1,
+                              outerEllipse.center(),
+                              cctagPoints,
+                              outerEllipse,
+                              markerHomography,
+                              pyramidLevel,
+                              scale,
+                              quality2 );
 #ifdef CCTAG_SERIALIZE
-      markers.back().setFlowComponents(componentCandidates);
+      tag->setFlowComponents( componentCandidates ); // markers.back().setFlowComponents(componentCandidates);
+#endif
+      markers.push_back( tag ); // markers takes responsibility for delete
+#ifdef CCTAG_SERIALIZE
 #ifdef DEBUG
+
       CCTagFileDebug::instance().outputFlowComponentAssemblingInfos(PASS_ALLTESTS);
       CCTagFileDebug::instance().incrementFlowComponentIndex(0);
 #endif
