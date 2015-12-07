67d79d87d11db8847b35c1127a77ac80a207ea32
Carsten Griwodz <griff@simula.no>
2015-12-07 12:45:09 +0100

    fix warning message

--- src/cuda/frame_01_tex.cu
+++ src/cuda/frame_01_tex.cu
@@ -40,7 +40,7 @@ void Frame::applyPlaneDownload( )
 {
 #if !defined(VISUAL_DEBUG)
     if( _layer != 0 ) return;
-#endif not defined(VISUAL_DEBUG)
+#endif // not defined(VISUAL_DEBUG)
 
     cudaStreamWaitEvent( _download_stream, _download_ready_event.plane, 0 );
 
