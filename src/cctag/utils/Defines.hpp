f057f9894783c4f6a1c82212701e5cec6dccf820
cyril_pih <pih@mikrosimage.eu>
2016-02-05 18:09:32 +0100

    moves System.hpp defines to Defines.hpp

--- src/cctag/utils/Defines.hpp
+++ src/cctag/utils/Defines.hpp
@@ -1,9 +1,50 @@
-#ifndef _CCTAG_GLOBAL_HPP_
-#define _CCTAG_GLOBAL_HPP_
+#ifndef _CCTAG_DEFINES_HPP_
+#define _CCTAG_DEFINES_HPP_
 
 #undef WITH_CUDA_COMPARE_MODE
 
-#include <cctag/utils/System.hpp>
+#if defined( linux ) \
+    || defined( __linux ) \
+    || defined( LINUX ) \
+    || defined( _LINUX ) \
+    || defined( __LINUX__ ) \
+
+ #ifndef __LINUX__
+  #define __LINUX__
+ #endif
+
+#elif defined( macintosh ) \
+    || defined( Macintosh ) \
+    || defined( __APPLE__ ) \
+    || defined( __MACH__ ) \
+    || defined( MACOS ) \
+    || defined( MACOSX ) \
+    || defined( __MACOS__ ) \
+
+ #ifndef __MACOS__
+  #define __MACOS__
+ #endif
+
+#elif defined( WIN32 ) \
+    || defined( _WIN32 ) \
+    || defined( __WIN32__ ) \
+    || defined( WIN64 ) \
+    || defined( _WIN64 ) \
+    || defined( __WIN64__ ) \
+    || defined( __TOS_WIN__ ) \
+    || defined( WINDOWS ) \
+    || defined( _WINDOWS ) \
+    || defined( __WINDOWS__ ) \
+
+ #ifndef __WINDOWS__
+  #define __WINDOWS__
+ #endif
+
+#else
+
+ #warning "Your operating system is not recognized."
+
+#endif
 
 ////////////////////////////////////////////////////////////////////////////////
 // Assert needs to be everywhere
