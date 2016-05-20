ed725064c19ef629157499f0af9b5fa7028aafc3
Zeljko Vrba <vrba@mixedrealities.no>
2016-05-20 10:54:13 +0200

    WIP: flattening out EdgePoint stuff.

--- src/cctag/EdgePoint.hpp
+++ src/cctag/EdgePoint.hpp
@@ -23,14 +23,16 @@ public:
     : Vector3s(0, 0, 1)
     , _grad(0.f,0.f)
     , _normGrad( 0.f )
-    , _before( NULL )
-    , _after( NULL )
+    , _before( -1 )
+    , _after( -1 )
+    , _votersBegin(-1)
+    , _votersEnd(-1)
     , _processed( 0 )
-    , _processedIn( false )
     , _isMax( -1 )
     , _nSegmentOut(-1)
     , _flowLength (0)
-    ,_processedAux(false)
+    , _processedAux(false)
+    , _processedIn( false )
   {}
   
   EdgePoint( const EdgePoint& p )
@@ -39,28 +41,31 @@ public:
     , _normGrad ( p._normGrad )
     , _before( p._before )
     , _after( p._after )
+    , _votersBegin(p._votersBegin)
+    , _votersEnd(p._votersEnd)
     , _processed( 0 )
-    , _processedIn( false )
     , _isMax( -1 )
     , _nSegmentOut(-1)
     , _flowLength (0)
     , _processedAux(false)
+    , _processedIn( false )
   {}
 
   EdgePoint( const int vx, const int vy, const float vdx, const float vdy )
     : Vector3s( vx, vy, 1 )
-    , _before( NULL )
-    , _after( NULL )
+    , _grad(vdx, vdy)
+    , _normGrad(std::sqrt( vdx * vdx + vdy * vdy ))
+    , _before( -1 )
+    , _after( -1 )
+    , _votersBegin(-1)
+    , _votersEnd(-1)
     , _processed( 0 )
-    , _processedIn( false )
     , _isMax( -1 )
     , _nSegmentOut(-1)
     , _flowLength (0)
     , _processedAux(false)
-    , _grad(vdx, vdy)
-    , _normGrad(0.f)
+    , _processedIn( false )
   {
-    _normGrad = std::sqrt( vdx * vdx + vdy * vdy );
   }
 
   Eigen::Vector2f gradient() const
@@ -85,19 +90,21 @@ public:
 
   friend std::ostream& operator<<( std::ostream& os, const EdgePoint& eP );
 
-  EdgePoint* _before;
-  EdgePoint* _after;
-  size_t _processed;    // bitfield; must be 64-bit
+  // XXX: optimize layout
+private:
+  Eigen::Vector2f _grad;
+  float _normGrad;
+public:
+  int _before;
+  int _after;
+  int _votersBegin;
+  int _votersEnd;
+  uint64_t _processed;   // bitfield; must be 64-bit
   int _isMax;
   int _nSegmentOut;     // std::size_t _nSegmentOut;
   float _flowLength;
   bool _processedAux;
   bool _processedIn;
-  std::vector<EdgePoint*> _voters;
-  
-private:
-  Eigen::Vector2f _grad;
-  float _normGrad;
 };
 
 inline bool receivedMoreVoteThan(const EdgePoint * const p1,  const EdgePoint * const p2)
--- src/cctag/Types.hpp
+++ src/cctag/Types.hpp
@@ -1,12 +1,10 @@
 #ifndef _CCTAG_MARKERS_TYPES_HPP_
 #define _CCTAG_MARKERS_TYPES_HPP_
 
+#include <stdexcept>
 #include <cctag/EdgePoint.hpp>
-
 #include <boost/multi_array.hpp>
 #include <boost/unordered/unordered_map.hpp>
-
-#include <list>
 
 
 namespace cctag {
@@ -18,6 +16,72 @@ typedef boost::multi_array<EdgePoint*, 2> EdgePointsImage;
 
 // typedef boost::unordered_map< EdgePoint*, std::vector< EdgePoint* > > WinnerMap;  ///< associate a winner with its voters
 
+class EdgePointCollection
+{
+public:
+  using int_vector = std::vector<int>;
+  using voter_list = std::pair<const int*, const int*>;
+  
+private:
+  std::vector<EdgePoint> _edgeList;
+  boost::multi_array<int,2> _edgeMap;
+  int_vector _voterLists;
+  EdgePoint *_currentVoteAdd;
+  
+public:
+  EdgePointCollection()
+  {
+    _edgeList.reserve(2 << 20);
+    _voterLists.reserve(6 << 20);
+    _currentVoteAdd = nullptr;
+  }
+  
+  ~EdgePointCollection()
+  {
+    if (_currentVoteAdd)
+      throw std::logic_error("EdgePointCollection: destructing while adding votes");
+  }
+  
+  // Cannot be const because fields are modified during edgelinking, etc.
+  EdgePoint& operator()(int i) { return _edgeList.at(i); }
+  EdgePoint& operator()(int i, int j) { return _edgeList.at(_edgeMap[i][j]); }
+  
+  // Used by CPU voting.
+  voter_list voters(const EdgePoint& p) const
+  {
+    if ((p._votersBegin < 0) != (p._votersEnd < 0))
+      throw std::logic_error("EdgePointCollection: invalid voter list indices (1)");
+    if (p._votersBegin < 0)
+      return std::make_pair(nullptr, nullptr);
+    if (p._votersBegin >= (int)_voterLists.size() || p._votersEnd > (int)_voterLists.size())
+      throw std::logic_error("EdgePointCollection: invalid voter list indices (2)");
+    return std::make_pair(_voterLists.data() + p._votersBegin, _voterLists.data() + p._votersEnd);
+  }
+  
+  // For CPU implementation of voting
+  void begin_add_votes(EdgePoint& p)
+  {
+    if (_currentVoteAdd)
+      throw std::logic_error("EdgePointCollection::begin_add_votes: already in progress");
+    _currentVoteAdd = &p;
+    p._votersBegin = _voterLists.size();
+  }
+  void end_add_votes(EdgePoint& p)
+  {
+    if (&p != _currentVoteAdd)
+      throw std::logic_error("EdgePointCollection::end_add_votes: invalid point");
+    _currentVoteAdd = nullptr;
+    p._votersEnd = _voterLists.size();
+  }
+  void add_vote(EdgePoint& p, int i)
+  {
+    if (&p != _currentVoteAdd)
+      throw std::logic_error("EdgePointCollection::add_vote: invalid point");
+    _voterLists.push_back(i);
+  }
+};
+
 } // namespace cctag
 
 #endif
+
