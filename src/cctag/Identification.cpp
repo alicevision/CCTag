849a62f0ebf19d8af979c0bd7f78fcd494e33e46
Carsten Griwodz <griff@simula.no>
2016-08-10 14:20:26 +0200

    undo hotfix

--- src/cctag/Identification.cpp
+++ src/cctag/Identification.cpp
@@ -1349,11 +1349,7 @@ int identify_step_1(
   )
 #endif
   
-  if ( outerPoints.size() < 5 )
-  {
-    // Can happen because points are retained too aggressively for higher accuracy
-    return status::too_few_outer_points;
-  }
+  assert ( outerPoints.size() >= 5 );
   
   // todo: next line deprec, associated to SUBPIX_EDGE_OPTIM, do not remove.
   const float cutLengthOuterPointRefine = std::min( ellipse.a(), ellipse.b() ) * 0.12;
