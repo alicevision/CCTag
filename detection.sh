5c1f00f1d2152a66bd410793b75ec080d59e178a
Carsten Griwodz <griff@simula.no>
2016-08-10 08:31:07 +0200

    test files does not work any more

--- detection.sh
+++
@@ -1,5 +0,0 @@
-# Example
-#build/src/detection -i sample/00003.jpg -b cctagLibraries/4Crowns/ids.txt -p parameters/param4.xml
-#build/src/detection -i /home/lilian/data/cctag/bench/videos/threeCrowns/video1/4.avi -n 4 -p parameters/param4.xml 
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-build/$branch/src/detection -i sample/00001.png -n 4 -p parameters/param4.xml
