d2d616da8ff18ffee531da924df4d95c19370f5b
Simone Gasparini <simone.gasparini@gmail.com>
2016-09-30 12:12:48 +0200

    [cmake] removed useless Find modules

--- cmake/FindUBlas.cmake
+++
@@ -1,8 +0,0 @@
-find_path(UBlas_INCLUDE_DIR boost/numeric/ublas)
-
-if(UBlas_INCLUDE_DIR)
-   set(UBlas_FOUND TRUE)
-   set(UBlas_DEFINITIONS -DWITH_UBlas)
-   message(STATUS "Found UBlas: ${UBlas_INCLUDE_DIR}")
-endif()
-
