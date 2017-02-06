9daf288ea945e8f4c177d8ef85444a9948ad1b8b
Fabien Castan <faca@mikrosimage.eu>
2017-02-06 10:18:06 +0100

    Use bigger max_resolution

--- src/cctag/Types.hpp
+++ src/cctag/Types.hpp
@@ -19,7 +19,7 @@ namespace cctag {
 class EdgePointCollection
 {
   static constexpr size_t MAX_POINTS = size_t(1) << 20;
-  static constexpr size_t MAX_RESOLUTION = 5120;
+  static constexpr size_t MAX_RESOLUTION = 6144;
   static constexpr size_t CUDA_OFFSET = 1024; // 4 kB, one page
   static constexpr size_t MAX_VOTERLIST_SIZE = 16*MAX_POINTS;
   
