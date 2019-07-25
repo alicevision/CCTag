c83bafabc47d037672db82755f51cc41b586258c
Fabien Castan <fabcastan@gmail.com>
2019-07-25 12:49:36 +0200

    Disable debug code to avoid build issues (#105)
    Fix debug function debugPointIsOnEdge() - it broke Debug compiles.
    Set default CMAKE_BUILD_TYPE to Release except for MSVC mode.

--- src/cctag/cuda/frame_06_graddesc.cu
+++ src/cctag/cuda/frame_06_graddesc.cu
@@ -399,7 +399,7 @@ bool Frame::applyDesc( )
     dim3 grid( grid_divide( listsize, 32 ), 1, 1 );
 
 #ifndef NDEBUG
-    debugPointIsOnEdge( _d_edges, _all_edgecoords, _stream );
+    debugPointIsOnEdge( _meta, _d_edges, _all_edgecoords, _stream );
 #endif
 
     descent::gradient_descent
