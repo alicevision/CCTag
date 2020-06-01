8c89362baa5e37eccbf973c72c90778d05d47404
Simone Gasparini <simone.gasparini@gmail.com>
2020-06-02 00:15:21 +0200

    [cctag] add doc Params.*

--- src/cctag/Params.cpp
+++ src/cctag/Params.cpp
@@ -6,14 +6,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 #include "Params.hpp"
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
+
 #include <boost/archive/xml_iarchive.hpp>
 #include <boost/filesystem.hpp>
 
-namespace cctag
-{
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+namespace cctag {
 
 bool Parameters::OverrideChecked = false;
 bool Parameters::OverrideLoaded = false;
@@ -21,78 +22,78 @@ Parameters Parameters::Override;
 
 void Parameters::LoadOverride()
 {
-  const char* path = std::getenv("CCTAG_PARAMETERS_OVERRIDE");
-  if (!path) path = "./CCTagParametersOverride.xml";
-  std::ifstream ifs(path);
-  if (!ifs)
-    return;
-  
-  boost::archive::xml_iarchive ia(ifs);
-  ia >> boost::serialization::make_nvp("CCTagsParams", Override);
-  OverrideLoaded = true;
-  std::cout << "CCTag: loaded parameters override file: " << path << std::endl;
+    const char* path = std::getenv("CCTAG_PARAMETERS_OVERRIDE");
+    if(!path)
+        path = "./CCTagParametersOverride.xml";
+    std::ifstream ifs(path);
+    if(!ifs)
+        return;
+
+    boost::archive::xml_iarchive ia(ifs);
+    ia >> boost::serialization::make_nvp("CCTagsParams", Override);
+    OverrideLoaded = true;
+    std::cout << "CCTag: loaded parameters override file: " << path << std::endl;
 }
 
 Parameters::Parameters(std::size_t nCrowns)
-    : _cannyThrLow( kDefaultCannyThrLow )
-    , _cannyThrHigh( kDefaultCannyThrHigh )
-    , _distSearch( kDefaultDistSearch )
-    , _thrGradientMagInVote( kDefaultThrGradientMagInVote )
-    , _angleVoting( kDefaultAngleVoting )
-    , _ratioVoting( kDefaultRatioVoting )
-    , _averageVoteMin( kDefaultAverageVoteMin )
-    , _thrMedianDistanceEllipse( kDefaultThrMedianDistanceEllipse )
-    , _maximumNbSeeds( kDefaultMaximumNbSeeds )
-    , _maximumNbCandidatesLoopTwo( kDefaultMaximumNbCandidatesLoopTwo )
-    , _nCrowns( nCrowns )
-    , _minPointsSegmentCandidate( kDefaultMinPointsSegmentCandidate )
-    , _minVotesToSelectCandidate( kDefaultMinVotesToSelectCandidate )
-    , _threshRobustEstimationOfOuterEllipse( kDefaultThreshRobustEstimationOfOuterEllipse )
-    , _ellipseGrowingEllipticHullWidth( kDefaultEllipseGrowingEllipticHullWidth )
-    , _windowSizeOnInnerEllipticSegment( kDefaultWindowSizeOnInnerEllipticSegment )
-    , _numberOfMultiresLayers( kDefaultNumberOfMultiresLayers )
-    , _nSamplesOuterEllipse( kDefaultNSamplesOuterEllipse )
-    , _numberOfProcessedMultiresLayers( kDefaultNumberOfProcessedMultiresLayers )
-    , _numCutsInIdentStep( kDefaultNumCutsInIdentStep )
-    , _numSamplesOuterEdgePointsRefinement( kDefaultNumSamplesOuterEdgePointsRefinement )
-    , _cutsSelectionTrials( kDefaultCutsSelectionTrials )
-    , _sampleCutLength( kDefaultSampleCutLength )
-    , _imagedCenterNGridSample( kDefaultImagedCenterNGridSample )
-    , _imagedCenterNeighbourSize( kDefaultImagedCenterNeighbourSize )
-    , _minIdentProba( kDefaultMinIdentProba )
-    , _useLMDif( kDefaultUseLMDif )
-    , _searchForAnotherSegment( kDefaultSearchForAnotherSegment )
-    , _writeOutput( kDefaultWriteOutput )
-    , _doIdentification( kDefaultDoIdentification )
-    , _maxEdges( kDefaultMaxEdges )
-    , _useCuda( kDefaultUseCuda )
-    , _debugDir( "" )
+  : _cannyThrLow(kDefaultCannyThrLow)
+  , _cannyThrHigh(kDefaultCannyThrHigh)
+  , _distSearch(kDefaultDistSearch)
+  , _thrGradientMagInVote(kDefaultThrGradientMagInVote)
+  , _angleVoting(kDefaultAngleVoting)
+  , _ratioVoting(kDefaultRatioVoting)
+  , _averageVoteMin(kDefaultAverageVoteMin)
+  , _thrMedianDistanceEllipse(kDefaultThrMedianDistanceEllipse)
+  , _maximumNbSeeds(kDefaultMaximumNbSeeds)
+  , _maximumNbCandidatesLoopTwo(kDefaultMaximumNbCandidatesLoopTwo)
+  , _nCrowns(nCrowns)
+  , _minPointsSegmentCandidate(kDefaultMinPointsSegmentCandidate)
+  , _minVotesToSelectCandidate(kDefaultMinVotesToSelectCandidate)
+  , _threshRobustEstimationOfOuterEllipse(kDefaultThreshRobustEstimationOfOuterEllipse)
+  , _ellipseGrowingEllipticHullWidth(kDefaultEllipseGrowingEllipticHullWidth)
+  , _windowSizeOnInnerEllipticSegment(kDefaultWindowSizeOnInnerEllipticSegment)
+  , _numberOfMultiresLayers(kDefaultNumberOfMultiresLayers)
+  , _nSamplesOuterEllipse(kDefaultNSamplesOuterEllipse)
+  , _numberOfProcessedMultiresLayers(kDefaultNumberOfProcessedMultiresLayers)
+  , _numCutsInIdentStep(kDefaultNumCutsInIdentStep)
+  , _numSamplesOuterEdgePointsRefinement(kDefaultNumSamplesOuterEdgePointsRefinement)
+  , _cutsSelectionTrials(kDefaultCutsSelectionTrials)
+  , _sampleCutLength(kDefaultSampleCutLength)
+  , _imagedCenterNGridSample(kDefaultImagedCenterNGridSample)
+  , _imagedCenterNeighbourSize(kDefaultImagedCenterNeighbourSize)
+  , _minIdentProba(kDefaultMinIdentProba)
+  , _useLMDif(kDefaultUseLMDif)
+  , _searchForAnotherSegment(kDefaultSearchForAnotherSegment)
+  , _writeOutput(kDefaultWriteOutput)
+  , _doIdentification(kDefaultDoIdentification)
+  , _maxEdges(kDefaultMaxEdges)
+  , _useCuda(kDefaultUseCuda)
+  , _debugDir("")
 {
-    _nCircles = 2*_nCrowns;
-    
-    if (!OverrideChecked) {
-      OverrideChecked = true;
-      LoadOverride();
+    _nCircles = 2 * _nCrowns;
+
+    if(!OverrideChecked)
+    {
+        OverrideChecked = true;
+        LoadOverride();
     }
 }
 
-void Parameters::setDebugDir( const std::string& debugDir )
+void Parameters::setDebugDir(const std::string& debugDir)
 {
-
-
-	namespace fs = boost::filesystem;
-	fs::path directory(debugDir);
-	if (fs::exists(directory))
-	{
-		std::cout << "Directory " << debugDir << " already exists.\n";
-	}
-	else
-	{
-		fs::create_directories(directory);
-	}
+    namespace fs = boost::filesystem;
+    fs::path directory(debugDir);
+    if(fs::exists(directory))
+    {
+        std::cout << "Directory " << debugDir << " already exists.\n";
+    }
+    else
+    {
+        fs::create_directories(directory);
+    }
 }
 
-void Parameters::setUseCuda( bool val )
+void Parameters::setUseCuda(bool val)
 {
 #ifdef CCTAG_WITH_CUDA
     _useCuda = val;
