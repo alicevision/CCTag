79502d9e86aecfe1063a0983d66e1ceffe1aaac1
Simone Gasparini <simone.gasparini@gmail.com>
2020-06-02 00:51:23 +0200

    [doc] use doxygen in RTD

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -14,6 +14,31 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import subprocess, os
+
+def configure_doxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+breathe_projects = {}
+
+if read_the_docs_build:
+    # run doxygen before to generate the xml
+    output_dir = '../build'
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['CCTag'] = output_dir + '/xml'
+
+
+
 
 # -- Project information -----------------------------------------------------
 
